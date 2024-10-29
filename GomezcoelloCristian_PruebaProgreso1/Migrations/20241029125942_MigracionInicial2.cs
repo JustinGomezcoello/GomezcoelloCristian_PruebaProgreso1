5f5fb690d8ca9e672720468df29e9d22eb7c6118
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-10-29 08:03:38 -0500

    fafasf

---
+++ GomezcoelloCristian_PruebaProgreso1/Migrations/20241029125942_MigracionInicial2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GomezcoelloCristian_PruebaProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DireccionDate",
+                table: "Gomezcoello",
+                newName: "Fecha");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Edad",
+                table: "Gomezcoello",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Salario",
+                table: "Gomezcoello",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salario",
+                table: "Gomezcoello");
+
+            migrationBuilder.RenameColumn(
+                name: "Fecha",
+                table: "Gomezcoello",
+                newName: "DireccionDate");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Edad",
+                table: "Gomezcoello",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
