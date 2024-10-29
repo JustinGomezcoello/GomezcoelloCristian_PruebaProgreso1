fb86939130a75c9be4a5d83d3c00cfae21c6570a
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-10-29 08:33:27 -0500

    agregue boton

--- GomezcoelloCristian_PruebaProgreso1/wwwroot/js/site.js
+++ GomezcoelloCristian_PruebaProgreso1/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function CrearAlerta() {
+    alert("Mi nombre es Justin Gomezcoello y mi Hobbie es: Jugar videojuegos y ir al gym")
+}
