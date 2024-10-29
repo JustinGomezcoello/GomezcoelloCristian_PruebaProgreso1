c7c4232d1882eecd855e9c7a2fe5a0bd0774ffc1
laboratorio <laboratorio@UPOAULA10303>
2024-10-29 07:34:08 -0500

    Agregar archivos de proyecto.

---
+++ GomezcoelloCristian_PruebaProgreso1/Models/Celular.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GomezcoelloCristian_PruebaProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Modelo { get; set; }
+        [Required]
+        [Range(2000,2025)]
+        public int Ano { get; set; }
+        [NotNull]
+        [Display(Name = "Precio")]
+        public int Precio { get; set; }
+    }
+}
+ 
---
+++ GomezcoelloCristian_PruebaProgreso1/Models/Gomezcoello.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GomezcoelloCristian_PruebaProgreso1.Models
+{
+    public class Gomezcoello
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Range(0, 95)]
+        public double Edad { get; set; }
+        [Required]
+        public int Hermanos { get; set; }
+
+        [Display(Name = "Dirección")]
+        [MaxLength(200)]
+        public string Direccion { get; set; }
+        [NotNull]
+        public Boolean Quiteno { get; set; }
+        public DateTime? DireccionDate { get; set; }
+
+        public Celular? Celular { get; set; }
+        [ForeignKey("Celular")]
+        public int IdCelular { get; set; }
+
+
+    }
+}
